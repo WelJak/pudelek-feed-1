69ee77dbc450f33c5c087045ce11ded28019e8c5
WelJak <WelJak123@gmail.com>
2019-08-26 20:39:50 +0200

    updated scrapper.py - which now contains function that returns list of all articles on pudelek.pl

--- scrapper.py
+++ scrapper.py
@@ -1,34 +1,34 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
 
+def get_list_of_entries():
 
-url = 'https://www.pudelek.pl'
-client = uReq(url)
-page_html = client.read()
-client.close()
-page_soup = soup(page_html, "html.parser")
-entries = page_soup.findAll("div", {"class":"entry"})
+    url = 'https://www.pudelek.pl'
+    client = uReq(url)
+    page_html = client.read()
+    client.close()
+    page_soup = soup(page_html, "html.parser")
+    entries = page_soup.findAll("div", {"class":"entry"})
 
-filename = "entries.csv"
-f = open(filename, "w")
 
-headers = 'id; add_time; title; description; tags; links \n'
+    output = []
+    for entry in entries:
+        tag = ''
+        entry_id = entry["data-id"]
+        time = entry.span.text
+        title = entry.a.text.strip()
+        desc = entry.p.text
+        a = entry.find("span", {"class":"inline-tags"})
+        children = a.findChildren("a", recursive=False)
+        for i in range(len(children)):
+            tag = tag + '/' + children[i].text.strip()
+        for a in entry.find_all('a', href=True):
+            links = a['href']
+        part = (entry_id, time, title, desc, tag, links)
+        output.append(part)
+    return output
 
-f.write(headers)
 
-for entry in entries:
-    tag = ''
-    entry_id = entry["data-id"]
-    time = entry.span.text
-    title = entry.a.text.strip()
-    desc = entry.p.text
-    a = entry.find("span", {"class":"inline-tags"})
-    children = a.findChildren("a", recursive=False)
-    for i in range(len(children)):
-        tag = tag + '/' + children[i].text.strip()
-    for a in entry.find_all('a', href=True):
-        links = a['href']
-    f.write(entry_id + "; " + time + "; " + title + "; " + desc + "; " + tag + "; " + links + "\n")
-
-f.close()
+if __name__ == '__main__':
+    print(get_list_of_entries())
 
