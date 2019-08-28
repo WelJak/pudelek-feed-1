e0217b04e701b5ed38ba46534a36e359f5f6a40b
WelJak <WelJak123@gmail.com>
2019-08-28 14:43:30 +0200

    added class which checks if message has been already sent

---
+++ pudelek-feed/rabbitmq_checker/checker.py
@@ -0,0 +1,26 @@
+
+class InMemoryBase:
+    def check(self):
+        pass
+
+    def mark(self):
+        pass
+
+
+class InMemory(InMemoryBase):
+    def __init__(self, entries):
+        self.entries = list(entries)
+
+    def check(self, entry):
+        if entry in self.entries:
+            print('message was already sent')
+            return False
+        else:
+            print('message has not been sent yet')
+            return True
+
+    def mark(self, entry):
+        self.entries.append(entry)
+        print('message added to list of already sent messages')
+
+
