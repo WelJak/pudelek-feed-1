4ff94cae119d9a31f01d8c4f31906faf1e17ce3f
WelJak <WelJak123@gmail.com>
2019-08-29 15:47:01 +0200

    changed app.py

--- pudelekfeed/app.py
+++ pudelekfeed/app.py
@@ -3,23 +3,30 @@ from pudlas.pudelekfeed.checker.inmemory_checker import InMemoryChecker
 from pudlas.pudelekfeed.rabbitmq_producer.rabbitmq_producer import RabbitmqProducer
 import uuid
 import json
+import time
+import sys
+import traceback
 
-scrapper = Scrapper()
-checker = InMemoryChecker()
-producer = RabbitmqProducer('admin', 'admin', 'localhost', '', 'PUDELEK', 'pudelek-feed')
+try:
+    scrapper = Scrapper()
+    checker = InMemoryChecker()
+    producer = RabbitmqProducer('admin', 'admin', 'localhost', '', 'PUDELEK', 'pudelek-feed')
 
-# tu sie wrzuci petle
-entries = scrapper.fetch_list_of_entries()
+    while 1:
+        entries = scrapper.fetch_list_of_entries()
 
-for entry in entries:
-    if checker.check(entry) == True:
-        checker.mark(entry)
-        entry = {'uuid': str(uuid.uuid1()), 'type': 'Pudelek', 'message': entry}
-        entry = json.dumps(entry, ensure_ascii=False)
-        producer.send_message(entry)
+        for entry in entries:
+            if checker.check(entry) == True:
+                checker.mark(entry)
+                entry = {'uuid': str(uuid.uuid1()), 'type': 'Pudelek', 'message': entry}
+                entry = json.dumps(entry, ensure_ascii=False)
+                producer.send_message(entry)
 
-    else:
-        pass
-    # tu waita sie wrzuci
-    # tu koniec petli
-producer.connection_close() #z tym nie wiem co w petli zrobic - bo jakos w koncu trzeba bedzie zamknac to polaczenie
+            else:
+                pass
+
+        time.sleep(120)
+
+except:
+    print('An error occurred during process:')
+    traceback.print_exc(file=sys.stdout)
