adedee3f859b60ed428c334910ba40f622878fdf
WelJak <WelJak123@gmail.com>
2019-09-16 18:25:21 +0200

    added mocked communication with wykop for further development - wip

--- wykopproducer/app.py
+++ wykopproducer/app.py
@@ -19,23 +19,55 @@ DEFAULT_PROFILE = "LOCAL"
 mock = mock.Mock()
 WYKOP_APP_KEY = '123'
 WYKOP_SECRET_KEY = '321'
+WYKOP_USER_KEY = 'abcd'
 WYKOP_ADD_ENTRY_URL = 'https://a2.wykop.pl/Entries/Entry/entry/'
+WYKOP_ADD_LINK_URL = 'https://a2.wykop.pl/Addlink/Add/'
+WYKOP_LINK_DRAFT_URL = 'https://a2.wykop.pl/Addlink/Draft/'
 
 
 class App:
     def client(self, message):
         self.requests = mock
+        json = mock
         try:
-            headers = {
-                'apisign': self.create_md5checksum(WYKOP_SECRET_KEY, WYKOP_ADD_ENTRY_URL + 'appkey/' + WYKOP_APP_KEY),
+            link_draft_headers = {
+                'apisign': self.create_md5checksum(WYKOP_SECRET_KEY, WYKOP_LINK_DRAFT_URL+'appkey/'+WYKOP_APP_KEY+'/userkey/'+WYKOP_USER_KEY),
+                'Content-type': 'application/x-www-form-urlencoded'
+            }
+            link_draft_post_params = {'url': message['message']['link']}
+            wykop_link_draft_client = self.requests.post(
+                WYKOP_SECRET_KEY, WYKOP_LINK_DRAFT_URL + 'appkey/' + WYKOP_APP_KEY + '/userkey/' + WYKOP_USER_KEY,
+                data=link_draft_post_params,
+                headers=link_draft_headers
+            )
+            draft_response = json.loads(wykop_link_draft_client.json())
+
+            key = ''  # patrz dokumentacja
+            addlink_headers = {
+                'apisign': self.create_md5checksum(WYKOP_SECRET_KEY,
+                                                   WYKOP_ADD_LINK_URL + 'key/' + key + 'appkey/' + WYKOP_APP_KEY + '/userkey/' + WYKOP_USER_KEY),
+                'Content-type': 'application/x-www-form-urlencoded'
+            }
+            addlink_post_params = {'title': message['message']['title'],
+                                   'descritpion': message['message']['description'],
+                                   'tags': ', '.join(['message']['tags']),
+                                   'photo': draft_response['data']['compact']['photos']['key'],
+                                   'url': message['message']['link'],
+                                   'plus18': True}
+            wykop_addlink_client = self.requests.post(
+                WYKOP_ADD_LINK_URL + 'key/' + key + 'appkey/' + WYKOP_APP_KEY + '/userkey/' + WYKOP_USER_KEY,
+                data=addlink_post_params, headers=addlink_headers)
+            wykop_addlink_respone = json.loads(wykop_addlink_client.json())
+
+            addentry_headers = {
+                'apisign': self.create_md5checksum(WYKOP_SECRET_KEY,
+                                                   WYKOP_ADD_ENTRY_URL + 'appkey/' + WYKOP_APP_KEY + '/userkey/' + WYKOP_USER_KEY),
                 'Content-type': 'application/x-www-form-urlencoded'}
-            post_params = {'body': message, 'embed': '', 'adultmedia': False}
-            wykop_client = self.requests.post(WYKOP_ADD_ENTRY_URL + 'appkey/' + WYKOP_APP_KEY, data=post_params,
-                                              headers=headers)
-            if wykop_client.status_code == 200:
-                logger.info('Added new entry on wykop.pl')
-            elif wykop_client.status_code == 404:
-                logger.info('Response code 404 - posting new entry was unsuccessful')
+            addentry_post_params = {'body': 'Patrzcie co znalazlem '+ wykop_addlink_respone['data']['compact']['full']['url'], 'embed': '', 'adultmedia': False}
+            wykop_entry_client = self.requests.post(
+                WYKOP_ADD_ENTRY_URL + 'appkey/' + WYKOP_APP_KEY + '/userkey/' + WYKOP_USER_KEY,
+                data=addentry_post_params,
+                headers=addentry_headers)
         except:
             logger.info('An error occurred during sending message to wykop.pl')
             traceback.print_exc(file=sys.stdout)
