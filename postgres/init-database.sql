b2df19c72fe4a8f6918ae7cbcb26a5f64f71ac01
solveretur <przemyslaw.d.brzezinski@gmail.com>
2019-09-26 19:33:56 +0200

    spring boot storage service

--- postgres/init-database.sql
+++ postgres/init-database.sql
@@ -1,12 +1,12 @@
 --SCRIPT WHICH INITIALISE pudelekfeed DATABASE
-CREATE TABLE news (
-    uuid varchar PRIMARY KEY UNIQUE NOT NULL ,
-    type varchar NOT NULL ,
-    entry_id varchar NOT NULL UNIQUE ,
-    add_date varchar NOT NULL ,
-    title varchar NOT NULL ,
-    description varchar NOT NULL ,
-    tags varchar NOT NULL ,
-    link varchar NOT NULL
-
+CREATE TABLE news
+(
+    uuid        varchar PRIMARY KEY NOT NULL UNIQUE,
+    type        varchar             NOT NULL,
+    entry_id    varchar             NOT NULL UNIQUE,
+    add_date    varchar             NOT NULL,
+    title       varchar             NOT NULL,
+    description varchar             NOT NULL,
+    tags        varchar             NOT NULL,
+    link        varchar             NOT NULL
 );
