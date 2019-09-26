3164b01686acedc14af6459d743ab249cd4a744c
WelJak <WelJak123@gmail.com>
2019-09-26 16:41:08 +0200

    modified init-database.sql which now creates table to store messages from pudelek

--- postgres/init-database.sql
+++ postgres/init-database.sql
@@ -1,1 +1,12 @@
 --SCRIPT WHICH INITIALISE pudelekfeed DATABASE
+CREATE TABLE entries (
+    uuid varchar PRIMARY KEY ,
+    type varchar NOT NULL ,
+    entry_id varchar NOT NULL ,
+    add_date varchar NOT NULL ,
+    title varchar NOT NULL ,
+    description varchar NOT NULL ,
+    tags varchar NOT NULL ,
+    link varchar NOT NULL
+
+);
