69ed44dfdb20d3558a6fcbd89ecdfb6e6060fc19
WelJak <WelJak123@gmail.com>
2019-10-09 10:22:15 +0200

    wip

--- storage-service/src/main/java/com/weljak/storageservice/news/Tags.java
+++ storage-service/src/main/java/com/weljak/storageservice/news/Tags.java
@@ -4,12 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-<<<<<<< HEAD:storage-service/src/main/java/com/weljak/storageservice/message/Tags.java
-import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-=======
->>>>>>> 635806b779eda857df12dd7db05cbafcc80e28bb:storage-service/src/main/java/com/weljak/storageservice/news/Tags.java
 
 import javax.persistence.*;
 
@@ -29,15 +23,7 @@ public class Tags {
     @Column(name = "tag", nullable = false)
     private String tag;
 
-<<<<<<< HEAD:storage-service/src/main/java/com/weljak/storageservice/message/Tags.java
-    @ManyToOne(fetch = FetchType.EAGER)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @JoinColumn(name = "uuid", nullable = false)
-    private News uuid;
-
-
-=======
     @Column(name = "uuid")
     private String uuid;
->>>>>>> 635806b779eda857df12dd7db05cbafcc80e28bb:storage-service/src/main/java/com/weljak/storageservice/news/Tags.java
+
 }
