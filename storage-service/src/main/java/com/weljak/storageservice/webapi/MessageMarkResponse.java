b07b4b9774e97f25fb291bd74a3b298af94ce5f3
WelJak <WelJak123@gmail.com>
2019-10-15 14:11:50 +0200

    wip

--- storage-service/src/main/java/com/weljak/storageservice/webapi/MessageMarkResponse.java
+++ storage-service/src/main/java/com/weljak/storageservice/webapi/MessageMarkResponse.java
@@ -1,8 +1,10 @@
 package com.weljak.storageservice.webapi;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 @Value
 public class MessageMarkResponse {
-    private boolean sent_successfully;
+    @JsonProperty
+    private boolean sentSuccessfully;
 }
